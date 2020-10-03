d52a0cc4b07b49b1c32c0ba59d3a9366c9560047
Loading <loading@Loadings-MacBook-Pro.local>
2020-10-03 16:07:27 +1000

    App componment structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import uuid from "uuid";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
@@ -8,7 +8,7 @@ import TodoItem from "./components/TodoItem";
 
 export default class App extends Component {
   state = {
-    item: [
+    items: [
       {
         id: 1,
         title: "wake up",
@@ -18,17 +18,43 @@ export default class App extends Component {
         title: "sleep",
       },
     ],
-    // id:uuid()
-    id: uuidv4()
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    // console.log(e);
+  };
+  handleSubmit = (e) => {
+    // console.log(e);
+  };
+  clearList = () => {};
+  handleDelete = (id) => {
+    // console.log(`handle edit ${id}`);
+  };
+  handleEdit = (id) => {
+    // console.log(`edit edit ${id}`);
   };
   render() {
-    console.log(this.state);
+    // console.log(this.state);
     return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <TodoInput />
-            <TodoList />
+      <div className="container">
+        <div className="row">
+          <div classNAme="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit} 
+              editItem={this.state.editItem}
+            />
+            <TodoList 
+              items={this.state.items} 
+              clearList={this.clearList} 
+              handleDelete={this.handleDelete} 
+              handleEdit={this.handleEdit}
+            />
           </div>
         </div>
       </div>
